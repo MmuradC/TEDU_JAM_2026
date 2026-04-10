eca2f361569df674cb48578ba2336354419f25f3
Muhammed Murad Chamaa <141.muh.mur.cha@gmail.com>
2026-04-10 23:48:01 +0300

    Plane Update

--- name/1_entities/1_vehicle/plane.gd
+++ name/1_entities/1_vehicle/plane.gd
@@ -1,11 +1,9 @@
 extends RigidBody3D
 
-# Ayarlanabilir Değişkenler (Inspector panelinden değiştirebilirsin)
-@export var engine_power: float = 150.0  # Motorun itiş gücü
-@export var pitch_speed: float = 3.0     # Burnunu aşağı/yukarı eğme hızı
-@export var roll_speed: float = 3.0      # Sağa/sola yatma hızı
+@export var engine_power: float = 150.0  
+@export var pitch_speed: float = 3.0     
+@export var roll_speed: float = 3.0      
 
-# Düğümleri (Node) Koda Tanımlama
 @onready var thrust_point = $ThrustPoint
 @onready var ground_sensor = $GroundSensor
 
@@ -14,17 +12,22 @@ func _ready():
 
 func _physics_process(delta):
 	if Input.is_action_pressed("ui_up"):
-		var force_direction = Vector3.FORWARD * engine_power
-		apply_local_force(force_direction, thrust_point.position)
+		var local_force = Vector3.FORWARD * engine_power
+		var global_force = global_transform.basis * local_force
+		var global_pos_offset = global_transform.basis * thrust_point.position
+		
+		apply_force(global_force, global_pos_offset)
 
 	var pitch_input = Input.get_axis("ui_down", "ui_up") 
 	var roll_input = Input.get_axis("ui_right", "ui_left") 
 	
-	var torque = Vector3.ZERO
-	torque.x = pitch_input * pitch_speed
-	torque.z = roll_input * roll_speed
+	var local_torque = Vector3.ZERO
+	local_torque.x = pitch_input * pitch_speed  
+	local_torque.z = roll_input * roll_speed    
 	
-	apply_local_torque(torque)
+	var global_torque = global_transform.basis * local_torque
+	
+	apply_torque(global_torque)
 
 	if ground_sensor.is_colliding():
 		var hit_point = ground_sensor.get_collision_point()
